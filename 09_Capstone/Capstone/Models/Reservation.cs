9b91894ff12c8e8ab35b4d011b70cea30b50fa1a
Joel Birdsall <joel.birdsall@gmail.com>
2020-06-18 08:52:43 -0400

    Properties and Constructors updated for all Model classes

--- 09_Capstone/Capstone/Models/Reservation.cs
+++ 09_Capstone/Capstone/Models/Reservation.cs
@@ -4,7 +4,45 @@ using System.Text;
 
 namespace Capstone.Models
 {
-    class Reservation
+    public class Reservation
     {
+        //This class contains the definition of a single reservation
+
+        //---------- PROPERTIES ----------------------------------------------------------------------------------------------------------------------------------
+
+        public int ReservationID { get; }
+        public string VenueName { get; }
+        public string SpaceName { get; }
+        public string ReservationName { get; }
+        public int NumberOfAttendees { get; }
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+        public int ReservationLength { get; }
+        public int DailyRate { get; }
+
+        public int TotalReservationCost
+        {
+            get
+            {
+                return ReservationLength * DailyRate;
+            }
+        }
+
+        //---------- CONSTRUCTORS ------------------------------------------------------------------------------------------------------------------------
+
+        public Reservation(int reservationID, string venueName, string spaceName, string reservationName,
+            int numberOfAttendees, DateTime startDate, int reservationLength, int dailyRate)
+        {
+            ReservationID = reservationID;
+            VenueName = venueName;
+            SpaceName = spaceName;
+            ReservationName = reservationName;
+            NumberOfAttendees = numberOfAttendees;
+            StartDate = startDate;
+            ReservationLength = reservationLength;
+            DailyRate = dailyRate;
+            EndDate = startDate.AddDays(ReservationLength);
+            
+        }
     }
 }
--- 09_Capstone/Capstone/Models/Space.cs
+++ 09_Capstone/Capstone/Models/Space.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace Capstone.Models
 {
-    class Space
+    public class Space
     {
+        //---------- PROPERTIES ----------------------------------------------------------------------------------------------------------------------------------
+
+        public int SpaceID { get; }
+        public string SpaceName { get; }
+        public int DailyRate { get; }
+        public int MaxOccupancy { get; }
+        public bool Accessablity { get; }
+        public int TotalCost { get; }
+        public DateTime OpenFrom { get; }
+        public DateTime OpenTo { get; }
+
+        //---------- CONSTRUCTORS ------------------------------------------------------------------------------------------------------------------------
+
+        public Space(int spaceID, string spaceName, int dailyRate, int maxOccupancy, int accessability, int reservationLength, DateTime openFrom, DateTime openTo)
+        {
+            SpaceID = spaceID;
+            SpaceName = spaceName;
+            DailyRate = dailyRate;
+            MaxOccupancy = maxOccupancy;
+            Accessablity = accessability == 1;
+            TotalCost = reservationLength * DailyRate;
+            OpenFrom = openFrom;
+            OpenTo = openTo;
+
+        }
+
     }
+
+
 }
