80205e3a6adca98cec95d66da361f346ac8703eb
Joel Birdsall <joel.birdsall@gmail.com>
2020-06-18 07:01:54 -0400

    Added in 3 DAL classes, 3 model classes, and some properties to the reservation model

---
+++ 09_Capstone/Capstone/Models/Venue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    class Venue
+    {
+        //This class contains the definition of a single reservation
+
+        //---------- PROPERTIES ----------------------------------------------------------------------------------------------------------------------------------
+
+        public int ReservationID { get; }
+        public int SpaceID { get; }
+        public int NumberOfAttendees { get; }
+        public DateTime StartDate { get; } // This is where we need to figure out what the heck format we want to use for our dates
+        public DateTime EndDate { get; }   // I just put datetime in for now, and we'll most likely change later
+        public string ReservationName { get; }
+
+    }
+}
