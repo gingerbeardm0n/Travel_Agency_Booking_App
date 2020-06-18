a606c6a2e47ac413bbcf02e0ad3d3d14abdcc2a6
Eli Collinson <elicollinson1@gmail.com>
2020-06-18 08:49:27 -0400

    some minor SQL Query tweaks for the capstone

--- 09_Capstone/SampleCodeQueries.sql
+++ 09_Capstone/SampleCodeQueries.sql
@@ -1,8 +1,9 @@
+--Return Venues
 SELECT name FROM venue
 ORDER by name;
 
---Return a venue for viewing
-SELECT TOP 1 v.name, ci.name, ci.state_abbreviation, ca.name, v.description FROM venue v
+--Return a venue for viewing, may not need to join the state, handle category duplicates
+SELECT v.name, ci.name, ci.state_abbreviation, ca.name, v.description FROM venue v
 JOIN city ci ON v.city_id = ci.id
 JOIN state s ON ci.state_abbreviation = s.abbreviation
 JOIN category_venue cv ON v.id = cv.venue_id
@@ -10,7 +11,7 @@ JOIN category ca ON cv.category_id = ca.id
 WHERE v.name = 'Hidden Owl Eatery';
 
 --Return all reservations
-SELECT r.reservation_id, v.name, s.name, r.reserved_for, r.number_of_attendees, r.start_date, r.end_date, s.daily_rate FROM reservation r
+SELECT r.reservation_id, v.name venue_name, s.name space_name, s.id space_id, r.reserved_for, r.number_of_attendees, r.start_date, r.end_date, s.daily_rate FROM reservation r
 JOIN space s ON r.space_id = s.id
 JOIN venue v ON s.venue_id = v.id
 
@@ -25,4 +26,5 @@ WHERE start_date NOT BETWEEN '2020-06-15' AND '2020-07-03' AND end_date NOT BETW
 --Returns spaces in a venue that have a high enough max occupency and have no bookings between the dates, dates and venue will need to be paremeterized
 SELECT TOP 5 id, name, daily_rate, max_occupancy, is_accessible FROM space
 WHERE venue_id = 2 AND max_occupancy >= 30 
-AND id NOT IN(SELECT space_id FROM reservation WHERE (start_date BETWEEN '2020-06-15' AND '2020-07-03') OR (end_date BETWEEN '2020-06-15' AND '2020-07-03'))
+AND id NOT IN(SELECT space_id FROM reservation WHERE (start_date BETWEEN '2020-06-15' AND '2020-06-18') OR (end_date BETWEEN '2020-06-15' AND '2020-06-18') 
+OR (start_date < '2020-06-15' AND end_date > '2020-06-18'));
