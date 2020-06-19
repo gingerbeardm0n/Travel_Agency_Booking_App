545cde4f27aff5cd5e5786f1e210bfdb7d60ee08
Joel Birdsall <joel.birdsall@gmail.com>
2020-06-18 22:25:43 -0400

    small change to reservation test

--- 09_Capstone/Capstone.Tests/ReservationsDALTest.cs
+++ 09_Capstone/Capstone.Tests/ReservationsDALTest.cs
@@ -12,54 +12,20 @@ namespace Capstone.Tests
     public class ReservationsDALTests : ParentTest
     {
         [TestMethod]
-        public void TestIfInsertionToDataBase()
+        public void TestForInsertionToDataBase()
         {
             //----- Arrange -----------------------------------------------
 
             ReservationsDAL testObj = new ReservationsDAL(connectionString);
-            Reservation dummyReservation = new Reservation(4, "My Test Venue Name", "My Testy Space Name", "JoEli ColinSall", 2, 2050-01-01, 1, 500);
+            Reservation dummyReservation = new Reservation(4, "My Test Venue Name", "My Testy Space Name", "JoEli ColinSall", 2, new DateTime(2020,1,1), 1, 500);
+            
+            //----- Act ---------------------------------------------------
 
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-
-                string sqlInsertReservation
-
-
-
-                string sqlInsertVenue = "INSERT INTO venue (name, city_id, description) VALUES ('ZZZZ', 3, 'XXXX')";
-                string sqlGetInsertedVenueID = "SELECT id FROM venue WHERE name = 'ZZZZ'";
-                string sqlInsertCategories = "INSERT INTO category_venue (venue_id, category_id) VALUES (@id, 1)"
-                + "INSERT INTO category_venue (venue_id, category_id) VALUES (@id, 2);";
-
-                SqlCommand cmd = new SqlCommand(sqlInsertVenue, conn);
-                int count = cmd.ExecuteNonQuery();
-
-                cmd = new SqlCommand(sqlGetInsertedVenueID, conn);
-                SqlDataReader reader = cmd.ExecuteReader();
-                int newId = 0;
-                while (reader.Read())
-                {
-                    newId = Convert.ToInt32(reader["id"]);
-                }
-                reader.Close();
-                cmd = new SqlCommand(sqlInsertCategories, conn);
-                cmd.Parameters.AddWithValue("@id", newId);
-                cmd.ExecuteNonQuery();
-
-
-            }
-
-
-
-                //----- Act ---------------------------------------------------
-
-                testObj.AddReservationToDataBase(reservation);
-
+            Reservation testReservation = testObj.AddReservationToDataBase(dummyReservation);
 
             //----- Assert ------------------------------------------------
 
-
+            Assert.AreEqual(45, testReservation.ReservationID);
         }
 
 
